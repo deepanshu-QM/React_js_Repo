3a36e59466d44d521be64d4d1587cbb6734ad49f
deepanshu-QM <cdeepanshu231@gmail.com>
2025-11-01 15:55:37 +0530

    Counter Added

--- src/main.jsx
+++ src/main.jsx
@@ -1,49 +1,18 @@
 
 
-// Text-Area
+import { createRoot } from "react-dom/client";
+import { useState } from "react";
 
-import React, { useState } from 'react';
-import { createRoot } from 'react-dom/client';
+function App(){
+  const [count ,setCount] = useState(0);
 
-function TextArea() {
-  const [mytext, setmyText] = useState("");
-
-  function HandleUpdate(event) {
-    setmyText(event.target.value);
+  function onClickHandler(){
+    setCount(count+1);
   }
-
-  function HandleSubmit(event) {
-    event.preventDefault();
-    alert("Submitted Content: " + mytext);
-  }
-
-  const content = {
-    backgroundColor: "#c76719ff",
-    margin: "100px",
-    padding: "50px",
-    textAlign: "center",
-  };
-
-  const content2 = {
-    height: "100px",
-    width: "500px",
-    border: "solid 2px",
-  };
-
   return (
-    <form style={content} onSubmit={HandleSubmit}>
-      <label>
-        Write Something : <br />
-        <textarea
-          value={mytext}
-          onChange={HandleUpdate}
-          style={content2}
-        />
-        <p>Content : {mytext}</p>
-        <button type="submit">Submit</button>
-      </label>
-    </form>
-  );
+   <div>
+     <button onClick={onClickHandler}>Counter : {count}</button>
+   </div>
+  )
 }
-
-createRoot(document.getElementById('root')).render(<TextArea />);
+createRoot(document.getElementById('root')).render(<App/>)
