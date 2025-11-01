c4859336d2d79d8dc9e9f4486bacc23cb2dba742
deepanshu-QM <cdeepanshu231@gmail.com>
2025-11-01 17:23:01 +0530

    Resualble Counter Component

--- src/main.jsx
+++ src/main.jsx
@@ -4,15 +4,24 @@ import { createRoot } from "react-dom/client";
 import { useState } from "react";
 
 function App(){
-  const [count ,setCount] = useState(0);
-
-  function onClickHandler(){
-    setCount(count+1);
-  }
+  const [count , setCount] = useState(0);
   return (
-   <div>
-     <button onClick={onClickHandler}>Counter : {count}</button>
-   </div>
+    <div>
+      <CustomButton count={count} setCount={setCount}></CustomButton>
+    </div>
   )
 }
+
+//Component (Here I defining How mybutton Work)
+
+function CustomButton(props){
+
+  function onClickHandler(){
+    props.setCount(props.count + 1);
+  }
+  return (
+  <button onClick={onClickHandler}>Counter : {props.count}</button>
+  )
+}
+
 createRoot(document.getElementById('root')).render(<App/>)
